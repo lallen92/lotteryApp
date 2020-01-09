b9d1a7d9ca48bd190d39c8c9c1365b04cbdaf635
Liam Allen <liam.allen@rapttouch.com>
2020-01-09 17:41:21 +0000

    Load numbers for quick pick and manual, need to add code to check for duplicated values

--- src/com/rapttouch/lotteryApp/Main.java
+++ src/com/rapttouch/lotteryApp/Main.java
@@ -1,38 +1,170 @@
 package com.rapttouch.lotteryApp;
 
-import java.util.Scanner;
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.util.*;
 
 public class Main {
 
-    public static void main(String[] args) {
-        String welcomeMessage = "Welcome to Lotto Online!";
+    static int[] manualPick() {
+        String invalidEntry = "Invalid entry, please enter 6 valid numbers between 1-48!\n";
+        int manualPickArr[] = new int[6];
+        boolean inValidEntry = true;
+        int numberEntered = 0;
+
+        System.out.println("Enter 6 unique numbers between 1-48");
+        while (inValidEntry)
+        {
+            BufferedReader bi = new BufferedReader(new InputStreamReader(System.in));
+            String[] strNums = new String[0];
+
+            try
+            {
+                strNums = bi.readLine().split("\\s");
+            }
+            catch (IOException e)
+            {
+                inValidEntry = true;
+                System.out.println(invalidEntry);
+            }
+
+            if(strNums.length == 6)
+            {
+                for (int i = 0; i < strNums.length; i++)
+                {
+                    try
+                    {
+
+                        if ((Integer.parseInt(strNums[i]) >= 1) && (Integer.parseInt(strNums[i]) <= 48))
+                        {
+                                manualPickArr[i] = Integer.parseInt(strNums[i]);
+                                inValidEntry = false;
+                        }
+                        else
+                        {
+                            inValidEntry = true;
+                            System.out.println(invalidEntry);
+                            break;
+                        }
+
+                    }
+                    catch (NumberFormatException e) {
+                        inValidEntry = true;
+                        System.out.println(invalidEntry);
+                        break;
+                    }
+                    catch (ArrayIndexOutOfBoundsException e)
+                    {
+                        inValidEntry = true;
+                        System.out.println(invalidEntry);
+                        break;
+                    }
+                }
+            }
+            else
+            {
+                inValidEntry = true;
+                System.out.println(invalidEntry);
+            }
+        }
+        return manualPickArr;
+
+    }
+
+
+    static int[] quickPick()
+    {
+        int quickPickArr[] = new int[6];
+        int max = 48;
+        int min = 1;
+        for(int i = 0; i<=5; i++)
+            quickPickArr[i] =  getRandomInteger(max, min);
+        return quickPickArr;
+    }
+
+    private static int getRandomInteger(int maximum, int minimum) {
+        return ((int) (Math.random() * (maximum - minimum))) + minimum;
+    }
+
+    static int selectOption()
+    {
         String optionsMessage = new StringBuilder()
                 .append("For 'Quick Pick' selection, please enter the Number: 1\n")
                 .append("For manual selection, please the number: 2,\n")
                 .append("To exit the application, please press the number: 3").toString();
-        int[] possibleNumbers = {1, 2, 3};
-        boolean validEntry = false;
-        boolean chooseOption = true;
+
+        boolean inValidEntry = true;
         int numberEntered = 0;
 
-        System.out.println(welcomeMessage);
-
-        while (chooseOption)
+        while (inValidEntry)
         {
             System.out.println(optionsMessage);
             Scanner input = new Scanner(System.in);
-            numberEntered = input.nextInt();
-
-            for (int n : possibleNumbers) {
-                validEntry = (n == numberEntered);
-                if (validEntry) {
-                    chooseOption = false;
-                    break;
+            try
+            {
+                numberEntered = input.nextInt();
+                if ((numberEntered >=1) &&( numberEntered <= 3)) {
+                    inValidEntry = false;
+                }
+                else
+                {
+                    inValidEntry = true;
+                    System.out.println("Invalid entry, please enter a valid option!\n");
                 }
             }
-
-            System.out.println("Invalid entry, please enter a valid option!\n");
+            catch (InputMismatchException e)
+            {
+                inValidEntry = true;
+                System.out.println("Invalid entry, please enter a valid option!\n");
+            }
         }
-        System.out.println("You entered " + numberEntered);
+        System.out.println();
+        return numberEntered;
+    }
+
+    public static void main(String[] args) throws IOException {
+        int inputChoice = 0;
+        int returnedArr[] = new int[6];
+        int quickNumberLines = 0;
+        ArrayList<ArrayList<Integer>> listOLists = new ArrayList<ArrayList<Integer>>();
+        String welcomeMessage = "Welcome to Lotto Online!";
+        System.out.println(welcomeMessage);
+
+
+        inputChoice = selectOption();
+
+        switch (inputChoice) {
+            case 1:
+                System.out.println("Quick Pick");
+                while(quickNumberLines < 4)
+                {
+                    ArrayList<Integer> storeAllArrayQuick = new ArrayList<Integer>();
+                    returnedArr = quickPick();
+                    for (int i = 0; i < returnedArr.length; i++)
+                        storeAllArrayQuick.add(returnedArr[i]);
+                    listOLists.add(storeAllArrayQuick);
+                    quickNumberLines += 1;
+                }
+                break;
+            case 2:
+                System.out.println("Manual Pick");
+                ArrayList<Integer> storeAllArrayManual = new ArrayList<Integer>();
+                returnedArr = manualPick();
+                for (int i = 0; i < returnedArr.length; i++)
+                    storeAllArrayManual.add(returnedArr[i]);
+                listOLists.add(storeAllArrayManual);
+                break;
+            case 3:
+                System.out.println("Exit");
+                break;
+            default:
+                System.out.println("Exit");
+        }
+        //printLinesToScreen(listOLists);
+        System.out.println(Arrays.toString(listOLists.toArray()));
+
     }
 }
+
+
